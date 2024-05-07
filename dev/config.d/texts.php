098f48965e70c798ceb98d5601fd86ed05085742
Welington De Lima Olegario <wolegario49@gmail.com>
2024-05-07 14:40:11 -0300

    Adiciona os novos textos no ambiente Ref.: #2

---
+++ dev/config.d/texts.php
@@ -0,0 +1,16 @@
+<?php
+
+use \MapasCulturais\i;
+
+return [
+    "text:home-header.title" => i::__("Bem-vind@!"),
+    "text:home-header.description" => i::__("O Mapa Cultural de Natal é uma plataforma livre, colaborativa e interativa de mapeamento do cenário cultural natalense, além de ser um instrumento de governança digital no aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas culturais promovidas no município de Natal. É a partir dela que é garantida a estruturação de Sistemas de Informações e Indicadores culturais."),
+    "text:home-opportunities.description" => i::__("É aqui que você pode realizar inscrições e acessar o resultado de editais e oportunidades como oficinas, prêmios e concursos. Você também pode criar o seu próprio formulário e divulgar sua oportunidade para outros agentes culturais."),
+    "text:home-entities.events" => i::__("Quer saber quais os eventos culturais que estão ocorrendo em sua região? Basta fazer uma pesquisa a partir das ferramentas de busca. Além disso, como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente!"),
+    "text:home-entities.agents" => i::__("Aqui você pode encontrar todos os artistas, gestores e produtores que estão registrados na plataforma. É uma rede de agentes que estão envolvidos na cena cultural da região. Você também pode realizar o seu cadastro, bem como de seus coletivos, bandas, instituições e empresas das quais faça parte."),
+    "text:home-entities.projects" => i::__("Este é o local onde você pode encontrar leis de fomento,mostras, convocatórias e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma."),
+    "text:home-feature.description" => i::__("Confira os últimos destaques de cada uma das áreas."),
+    "text:home-register.description" => i::__("Faça seu cadastro e colabore com o Mapas Culturais Colabore com a plataforma livre, colaborativa e interativa de mapeamento do cenário cultural e instrumento de governança digital no aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas culturais promovidas pela Funcarte/Secult."),
+    'text:home-developers.description' => i::__("Alô desenvolvedores Além disso, Mapas Culturais é um software livre, criado em parceria entre a hacklab/, secretarias de cultura, organizações não governamentais, empresas e coletivos que investem na plataforma. Você pode contribuir para o seu desenvolvimento através do GitHub."),
+    "text:home-map.description" => i::__("Os agentes, os espaços e os eventos cadastrados contam com a geolocalização de seus endereços, encontre-os aqui:"),
+];
