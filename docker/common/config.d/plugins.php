8ec01dec82f5b90177fa8384591e78a48a5f8481
erleibiazzio <erleibiazzio@gamil.com>
2026-04-14 15:01:54 -0300

    Ajuista config do plugin Zammad

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -21,7 +21,7 @@ return [
                 'url' => 'https://suporte.mapasculturais.com.br/assets/chat/chat-no-jquery.min.js',    
                 'background' => '#F66968',
                 'title' => 'Duvidas? Fale conosco',
-                'chatId' => 2,
+                'chatId' => 3,
                 'instacacao' => 'mapacultural.natal.rn.gov.br',
                 'estado' => 'Rio Grande do Norte'
             ]
